f68c4b9cd70e6324a903aca8eb270d2aa571c6d3
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2017-05-02 21:34:12 +0300

    init material-ui and add theme

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import muiTheme from './theme';
 
 import rootReducer from './reducers';
 import App from './containers/app';
 import './index.css';
+
+// Needed for onTouchTap
+injectTapEventPlugin();
 
 const logger = createLogger();
 const store = createStore(
@@ -16,7 +23,9 @@ const store = createStore(
 
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+      <App />
+    </MuiThemeProvider>
   </Provider>
 ),
   document.getElementById('root')
